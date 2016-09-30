1208d31092531412f318624715bae42486b5fbdb
venkata.mutyala <venkata.mutyala@digital.bis.gov.uk>
2016-09-30 11:53:50 +0100

    1118 story - 1210 subtask - setup Rifs web project

---
+++ src/main/java/uk.gov.rifs/Application.java
@@ -0,0 +1,14 @@
+package uk.gov.rifs.application.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Spring Boot Application Started ............");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/uk.gov.rifs/application/model/Opportunity.java
@@ -0,0 +1,7 @@
+package uk.gov.rifs.application.model;
+
+/**
+ * Created by venkata on 30/09/16.
+ */
+public class Opportunity {
+}
