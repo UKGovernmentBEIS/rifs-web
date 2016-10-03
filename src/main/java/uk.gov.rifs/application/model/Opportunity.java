e09d5a627e2f8b54e157830980d29b95c666638e
BenKDishman <ben.dishman@digital.bis.gov.uk>
2016-10-03 13:54:11 +0100

    initial data work started.  Checked in for co-working.

--- src/main/java/uk.gov.rifs/application/model/Opportunity.java
+++ src/main/java/uk.gov.rifs/application/model/Opportunity.java
@@ -1,7 +1,60 @@
 package uk.gov.rifs.application.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDate;
+import uk.gov.rifs.application.model.OpportunityValueItem;
+import uk.gov.rifs.application.model.Description;
+import java.util.*;
+
 /**
  * Created by venkata on 30/09/16.
  */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Opportunity {
+
+    private Long id;
+    private String title;
+    //Needs to be a date
+    private String startDate;
+    //will be a list at some point
+    private OpportunityValueItem valueItem;
+    private List <Description> sections;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Opportunity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", startDate='" + startDate + '\'' +
+                '}';
+    }
+
 }
+
+
+
