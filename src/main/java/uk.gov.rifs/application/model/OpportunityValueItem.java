39f174e43572edb041cac5a014ab0b909ba16102
BenKDishman <ben.dishman@digital.bis.gov.uk>
2016-10-03 13:56:26 +0100

    gg

---
+++ src/main/java/uk.gov.rifs/application/model/OpportunityValueItem.java
@@ -0,0 +1,43 @@
+package uk.gov.rifs.application.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDate;
+
+/**
+ * Created by venkata on 30/09/16.
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpportunityValueItem {
+
+    private String unit;
+    private Double amount;
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public Double getAmount() {
+        return this.amount;
+    }
+
+    public void setUnit(String unit) {
+        this.amount = amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "OpportunityValueItem{" +
+               ", unit='" + unit + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
+
+}
+
+
+
