111e75d0faf644695175991726ff2d153623ce63
venkata.mutyala <venkata.mutyala@digital.bis.gov.uk>
2016-10-04 20:11:15 +0100

    GF 1118 adding resources

--- src/main/java/uk.gov.rifs/application/controller/OpportunityController.java
+++ src/main/java/uk.gov.rifs/application/controller/OpportunityController.java
@@ -35,4 +35,10 @@ public class OpportunityController {
         return "opportunity/opportunitydescription";
     }
 
+    @RequestMapping("/opportunityuserroles")
+    public String opportunityUserRoles(Model model) {
+
+        return "opportunity/opportunityuserroles";
+    }
+
 }
