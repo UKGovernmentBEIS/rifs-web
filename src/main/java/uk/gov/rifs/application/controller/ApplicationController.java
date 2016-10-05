ba38862794f13384a5c6e236229ab02ef44975f6
Doug Clinton <doug@wiredthing.com>
2016-10-05 09:05:41 +0100

    Rebased from master

--- src/main/java/uk/gov/rifs/application/controller/ApplicationController.java
+++ src/main/java/uk/gov/rifs/application/controller/ApplicationController.java
@@ -3,14 +3,12 @@ package uk.gov.rifs.application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ApplicationController {
 
     @RequestMapping("/opportunitylinks")
     public String showOpportunityLinks(Model model) {
-
         return "/application/opportunitylinks";
     }
 
